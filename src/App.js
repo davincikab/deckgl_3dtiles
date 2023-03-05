3e07472099e9811d71516a4e4281d25f99af8bba
davincikab <davidnganganjeri079@gmail.com>
2023-03-05 21:41:54 +0300

    project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
 import logo from './logo.svg';
 import './App.css';
+import MapComponent from './components/map';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({
+    activeDataset:'dataset2',
+    activeLayer: {name:'lod22', id:1570079, label:'Lod 2'},
+  });
+
+  const datasets = {
+    'dataset1':[
+      // {name:'lod0',id:0, label:'Lod 0'},
+      {name:'lod12', id:1569344	, label:'LOD 1.2'},
+      {name:'lod13', id:1569352, label:'LOD 1.3'},
+      {name:'lod22', id: 1569356, label:'LOD 2'},
+    ],
+    'dataset2':[
+      // {name:'lod0',id:0, label:'Lod 0'},
+      {name:'lod12', id:1570077, label:'Lod 1.2'},
+      {name:'lod13', id:1570078, label:'Lod 1.3'},
+      {name:'lod22', id:1570079, label:'Lod 2'},
+    ]
+  };
+
+  const handleOnClick = (layerId) => {
+    let layers = datasets[state.activeDataset];
+    let layer = layers.find(layer => layer.name === layerId);
+
+    setState({ ...state, activeLayer:layer })
+  }
+
+  const changeLayer = (dataset) => {
+    setState({
+      ...state,
+      activeDataset:dataset,
+      activeLayer:datasets[dataset][0]
+    });
+
+  }
+
+  let { activeLayer, activeDataset } = state;
+
+  console.log(activeDataset);
+  console.log(activeLayer);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='side-tab'>
+        {/* <div className='header-section'>
+          Layer Control
+        </div> */}
+        <div className='layer-toggler'>
+          <div className={activeDataset === 'dataset1' ? 'toggle-btn active' : 'toggle-btn'} onClick={() => changeLayer("dataset1")} >Dataset 1</div>
+          <div className={activeDataset === 'dataset2' ? 'toggle-btn active' : 'toggle-btn'} onClick={() => changeLayer("dataset2")} >Dataset 2</div>
+        </div>
+
+        <div className='body'>
+            <div className='layer-div'>
+
+              <div className='layers-list'>
+                {datasets.dataset1.map(layer => (
+                  <CustomInput 
+                    key={`dataset1-${layer.name}`} 
+                    name="dataset1" 
+                    {...layer} 
+                    layer={layer}
+                    activeLayer={activeLayer}
+                    handleOnClick={handleOnClick} 
+                  />)
+                )}
+              </div>
+
+            </div>
+            <div className='layer-div'>
+              {/* <div className='toggle-btn'>Dataset 2</div> */}
+
+              <div className='layers-list'>
+                {/* {datasets.dataset2.map(layer => (<CustomInput key={`dataset2-${layer.name}`} name="dataset2" {...layer} handleOnClick={handleOnClick} />)) } */}
+              </div>
+            </div>
+        </div>
+
+
+      </div>
+
+      <MapComponent 
+        layer={activeLayer} 
+        dataset={activeDataset}
+        layers={datasets[activeDataset]}
+      />
+
     </div>
   );
+}
+
+const CustomInput = ({name, handleOnClick, label, layer, activeLayer}) => {
+  return (
+    <div className='input-div'>
+      <input 
+        type="radio" 
+        name={name} 
+        id={layer.name}
+        onChange={() => handleOnClick(layer.name)} 
+        checked={layer.name === activeLayer.name ? true : false} 
+      />
+      <label htmlFor={layer.name}>{label}</label>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/map/index.js
@@ -0,0 +1,90 @@
+/// app.js
+import React, { useEffect } from 'react';
+import DeckGL from '@deck.gl/react';
+import {GeoJsonLayer} from '@deck.gl/layers';
+import { Map } from 'react-map-gl';
+
+import data from '../../assets/2d_data_dataset1/lod12_3d.json';
+
+
+import {CesiumIonLoader} from '@loaders.gl/3d-tiles';
+import {Tile3DLayer} from '@deck.gl/geo-layers';
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+    longitude: 4.495761, 
+    latitude:  51.96360,
+    zoom: 15,
+    pitch: 60,
+    bearing: 0
+};
+
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZGF1ZGk5NyIsImEiOiJjanJtY3B1bjYwZ3F2NGFvOXZ1a29iMmp6In0.9ZdvuGInodgDk7cv-KlujA';
+
+
+const MapComponent = (props) => {    
+
+    let geoLayer = new GeoJsonLayer({
+        id: 'geojson',
+        data,
+        opacity: 0,
+        stroked: false,
+        filled: true,
+        extruded: false,
+        wireframe: true,
+        // getElevation: f => Math.sqrt(f.properties.valuePerSqm) * 10,
+        getFillColor: [255, 0, 255],
+        getLineColor: [255, 255, 255],
+        pickable: true
+    });
+
+    useEffect(() => {
+        
+    })
+
+    let tiles = [props.layer].map(layer => {
+        return new Tile3DLayer({
+            id: `tile-3d-layer-${layer.id}`,
+            data: `https://assets.ion.cesium.com/${props.layer.id}/tileset.json?v=2`,
+            opacity:layer.id === props.layer.id ? 1 : 0,
+            loader: CesiumIonLoader,
+            loadOptions: {
+              'cesium-ion': {accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjMTkwNDhmZS02NTU5LTQyMWEtYWM4OS0yMzc4YWYzZjNiYzMiLCJpZCI6Mjk4MjIsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1OTI3NjY0MjV9.7rd4qA5VUQk4gdyoWU6fnF6gJxLcOj8iLJKQ_VVENNk'}
+            },
+            onTilesetLoad: (tileset) => {
+              // Recenter to cover the tileset
+                const {cartographicCenter, zoom} = tileset;
+                console.log(tileset);
+            },
+        });
+    });
+
+
+    const layers = [
+        geoLayer,
+        ...tiles
+    ];
+
+    let centers = { 
+        'dataset1':{ latitude:51.9632025359, longitude:4.4949385378 },
+        'dataset2':{ latitude:53.20919, longitude:6.54877 }, 
+    }
+    
+    console.log(props.layers);
+    return (
+        <DeckGL
+          initialViewState={{...INITIAL_VIEW_STATE, ...centers[props.dataset]}}
+          controller={true}
+          layers={layers} 
+        >
+            <Map 
+                mapboxAccessToken={MAPBOX_ACCESS_TOKEN} 
+                mapStyle="mapbox://styles/mapbox/dark-v9"
+            />
+        </DeckGL>
+    )
+    
+}
+
+
+export default MapComponent;
